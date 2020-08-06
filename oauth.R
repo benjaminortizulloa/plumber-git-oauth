ddc87cab141c1932daf5b276b36e1b90d552938c
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-08-06 17:58:50 -0500

    updated url

--- oauth.R
+++ oauth.R
@@ -15,7 +15,7 @@ function(code, req, res) {
   gr <- list(client_id = id, client_secret = secret, code = code)
   postres <- httr::POST(url, body = gr)
 
-  login_url <- paste0('http://localhost:8080/login?access_token=', httr::content(postres)$access_token)
+  login_url <- paste0('http://localhost:8080/#/login/', httr::content(postres)$access_token)
 
   res$status <- 303 # redirect
   res$setHeader("Location", login_url)

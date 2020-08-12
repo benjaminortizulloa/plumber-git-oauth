c40ffc8b367bc60858c7aa011c5212c1b4fff223
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-08-12 03:41:48 -0500

    prepping for heroku

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 ### .Renviron file
 
 ```
+RECON_URL_DEV = "http://localhost:8080"
+RECON_URL_PROD = "production-website"
 GITHUB_AUTH_ACCESSTOKEN_URL = 'https://github.com/login/oauth/access_token'
 CLIENT_ID = "github-provided-client-id"
 CLIENT_SECRET = "github-provided-client-secret"
+PG_AUTH = "postgres_important_string_connection"
 ```
 
 ### docker launch
--- git_oauth.R
+++ git_oauth.R
@@ -2,7 +2,7 @@ gitOauth <- function(code, req, res) {
   gr <- list(client_id = id, client_secret = secret, code = code)
   postres <- httr::POST(url, body = gr)
   
-  login_url <- paste0(web_dev, '/#/login/', httr::content(postres)$access_token)
+  login_url <- paste0(web_prod, '/#/login/', httr::content(postres)$access_token)
   
   res$status <- 303 # redirect
   res$setHeader("Location", login_url)
